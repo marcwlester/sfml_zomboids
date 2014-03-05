b1801af338008d73006edbe62ad3739921948aff
marcwlester <marc.w.lester@gmail.com>
2014-03-05 13:37:36 -0500

    testing views

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Zomboids");
+    sf::View camera(sf::FloatRect(400, 300, 800, 600));
+
+    window.setView(camera);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
